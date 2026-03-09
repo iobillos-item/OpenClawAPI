6a6a00e76a12163704a4261f22f11d88203a1671
Ivan Obillos <ivanobillos@yahoo.com>
2026-03-10 03:10:08 +0800

    Add DeleteUserAsync method to IUserService interface
    Updated the IUserService interface in IUserService.cs to include a new method, DeleteUserAsync. This method accepts an integer id and returns a Task<bool>, enabling asynchronous user deletion functionality.

--- Application/Interfaces/IUserService.cs
+++ Application/Interfaces/IUserService.cs
@@ -7,5 +7,6 @@ public interface IUserService
     Task<UserResponseDto> CreateUserAsync(CreateUserDto createUserDto);
     Task<IReadOnlyList<UserResponseDto>> GetUsersAsync();
     Task<UserResponseDto> UpdateUserAsync(int id, UpdateUserDto updateUserDto);
+    Task<bool> DeleteUserAsync(int id);
 }
     
